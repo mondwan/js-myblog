6011563b71d30396a91a7ca3fc1d6be5d9912401
Mond Wan <mondwan.1015@gmail.com>
2014-05-03 23:12:51 +0800

    Linking up our web server and the mongodb
    --File changes
    package.json
    - Library q, monogodb for linking
    - Library express-session, connect-monogo will be used for processing
      cookies
    static/Setting.json
    - Our database setting

---
+++ core/db/db.js
@@ -0,0 +1,33 @@
+//System library
+var util = require('util');
+var path = require('path');
+
+//3rd party library
+var mongodb = require('mongodb');
+var Q = require('q');
+
+//Custom library
+var projectRoot = path.join(__dirname, '..', '..');
+var setting = require(path.join(projectRoot, 'static', 'Setting.json'));
+
+module.exports = {
+    connect: function () {
+        var deferred = Q.defer();
+        var url = util.format(
+            'mongodb://%s:%d/%s',
+            setting.host,
+            setting.port,
+            setting.db
+        );
+
+        mongodb.connect(url, function (err, db) {
+            if (err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(db);
+            }
+        });
+
+        return deferred.promise;
+    }
+};
