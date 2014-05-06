bd30c3f18e025108e187d0bdf96e45770f5da358
Mond Wan <mondwan.1015@gmail.com>
2014-05-06 13:54:52 +0800

    Update webpage layout and start to implement registration
    --Overview
    1. Remove the template paragraph in legend box at home page
    2. Add name, id properties for those input tags inside the form at
    registration page
    3. Implement the registration by starting with parsing the upload
    contents

--- routes/reg.js
+++ routes/reg.js
@@ -3,9 +3,13 @@ var router = express.Router();
 
 /* GET register web page. */
 router.get('/', function (req, res) {
-    res.render('reg', {
-        title: 'Express',
-    });
+    res.render('reg');
+});
+
+/* POST handle register resquest */
+router.post('/', function (req, res) {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 module.exports = router;
